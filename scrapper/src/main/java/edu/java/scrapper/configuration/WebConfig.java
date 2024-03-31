02ed6998bdedec9f14faf1aacf6b515d14b1e093
chulkov-alex <chulkov-alex@yandex-team.ru>
2024-03-31 23:04:42 +0300

    hw7: cache + bucket

---
+++ scrapper/src/main/java/edu/java/scrapper/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package edu.java.scrapper.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RateLimiterInterceptor rateLimitInterceptor;
+
+    public WebConfig(RateLimiterInterceptor rateLimitInterceptor) {
+        this.rateLimitInterceptor = rateLimitInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor);
+    }
+}
